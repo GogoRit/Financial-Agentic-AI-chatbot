a11d948d2f13c0437a941c4b3faf0d9db034ad3d
gogo2710 <gm8189@rit.edu>
2025-01-19 14:34:34 -0500

    Files for personal portfolio project

---
+++ playground.py
@@ -0,0 +1,46 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import os
+from dotenv import load_dotenv
+import phi.api
+
+import phi 
+from phi.playground import Playground, serve_playground_app
+
+load_dotenv()
+
+phi.api = os.getenv("PHI_API_KEY")
+
+client = Groq(
+    api_key = os.environ.get("GROQ_API_KEY")
+)
+
+# Created a new agent for web search
+web_search_agent = Agent(
+    name = "Web Search Agent",
+    role = "Search the web for information",
+    model = Groq(id = "llama-3.3-70b-versatile"),
+    tools = [DuckDuckGo()],
+    instructions  = ["Always include the source of the information in the notes"],
+    show_tools_calls = True,
+    markdown = True
+)
+
+# Created a new agent for financial data
+financial_agent = Agent(
+    name = "Financial Agent",
+    model = Groq(id = "llama-3.3-70b-versatile"),
+    tools = [
+        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news = True)
+    ],
+    instructions  = ["Use tables to display the data", "Always include the source of the information in the notes"],
+    show_tools_calls = True,
+    markdown = True
+)
+
+app = Playground(agents = [web_search_agent, financial_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
